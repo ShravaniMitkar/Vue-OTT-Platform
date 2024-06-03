534c63f5dd9de39d3e8b19d5c440b9e1702a92d2
ShravaniMitkar <shravanimitkar443@gmail.com>
2024-06-03 08:27:34 +0530

    uploading project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!-- 
+<script setup></script>
+<template>
+</template>
+<style scoped></style> -->
+
+<script setup>
+import MoviesComponent from "./components/MoviesComponent.vue";
+// import KeyFeatures from "./components/KeyFeatures.vue";
+</script>
+
+<template>
+  <MoviesComponent />
+</template>
+
+<style scoped>
+/* .main-container {
+  width: 100%;
+  min-height: 100vh;
+} */
+</style>
+<!-- 
+<script setup>
+
+</script>
+<template></template>
+<style scoped></style> -->
